0c32558097d4eb4c5b2b6886a03c8ae9deb8a8cb
im-henri <haidakhenri@gmail.com>
2022-01-04 19:56:00 +0200

    3d cube update

--- cubeScript.js
+++ cubeScript.js
@@ -2,6 +2,13 @@ var cubeScene = null;
 var cube3d    = null;
 var cubeSceneOpacity = 1.0;
 
+var back  = null; // img of cube face
+var front = null; // img of cube face
+var left  = null; // img of cube face
+var right = null; // img of cube face
+// var top = null;
+// var down = null;
+
 var scrollPercent = 0;
 // min scroll percent to scroll up when cube is clicked
 var min_click_scroll_up = 0.1;
@@ -23,21 +30,33 @@ function initialize(){
     cubeScene = document.getElementsByClassName("cube3d_scene")[0];
     cube3d    = cubeScene.getElementsByClassName("cube3d")[0];
 
-    var back  = cube3d.getElementsByClassName("back")[0].getElementsByTagName("img")[0];
-    var front = cube3d.getElementsByClassName("front")[0].getElementsByTagName("img")[0];
-    var left  = cube3d.getElementsByClassName("left")[0].getElementsByTagName("img")[0];
-    var right = cube3d.getElementsByClassName("right")[0].getElementsByTagName("img")[0];
-    var top   = cube3d.getElementsByClassName("top")[0].getElementsByTagName("img")[0];
-    var down  = cube3d.getElementsByClassName("down")[0].getElementsByTagName("img")[0];
+    back  = cube3d.getElementsByClassName("back")[0].getElementsByTagName("img")[0];
+    front = cube3d.getElementsByClassName("front")[0].getElementsByTagName("img")[0];
+    left  = cube3d.getElementsByClassName("left")[0].getElementsByTagName("img")[0];
+    right = cube3d.getElementsByClassName("right")[0].getElementsByTagName("img")[0];
+    //top   = cube3d.getElementsByClassName("top")[0].getElementsByTagName("img")[0];
+    //down  = cube3d.getElementsByClassName("down")[0].getElementsByTagName("img")[0];
     
     cubeScene.addEventListener("mouseenter", function(){
         cubeScene.style.opacity = 1;
-        back.src  = "images/cubeBack.png";
-        front.src = "images/cubeBack.png";
-        left.src  = "images/cubeBack.png";
-        right.src = "images/cubeBack.png";
-        top.src   = "images/cubeBack.png";
-        down.src  = "images/cubeBack.png";
+        if (scrollPercent > min_click_scroll_up){
+            back.src  = "images/cubeUp.png";
+            front.src = "images/cubeUp.png";
+            left.src  = "images/cubeUp.png";
+            right.src = "images/cubeUp.png";
+            //top.src   = "images/cubeBack.png";
+            //down.src  = "images/cubeBack.png";
+        }
+        else{
+            back.src  = "images/cubeBack.png";
+            front.src = "images/cubeBack.png";
+            left.src  = "images/cubeBack.png";
+            right.src = "images/cubeBack.png";
+            //top.src   = "images/cubeBack.png";
+            //down.src  = "images/cubeBack.png";
+        }
+        
+        
     });
     cubeScene.addEventListener("mouseleave", function(){
         cubeScene.style.opacity = cubeSceneOpacity;
@@ -45,8 +64,8 @@ function initialize(){
         front.src = "images/cubeFace.png";
         left.src  = "images/cubeFace.png";
         right.src = "images/cubeFace.png";
-        top.src   = "images/cubeFace.png";
-        down.src  = "images/cubeFace.png";
+        //top.src   = "images/cubeFace.png";
+        //down.src  = "images/cubeFace.png";
     });
 
     cubeScene.addEventListener("click", function(){
@@ -71,6 +90,10 @@ function on_scroll(value){
 function on_cube_click(){
     if (scrollPercent > min_click_scroll_up){
         window.scrollTo({top: 0, behavior: 'smooth'});
+        back.src  = "images/cubeBack.png";
+        front.src = "images/cubeBack.png";
+        left.src  = "images/cubeBack.png";
+        right.src = "images/cubeBack.png";
     }
     else{
         history.back();
